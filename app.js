596295327239dfb6f3502c8708ac4aa52316eeb6
loji4 <yogawithlindao@gmail.com>
2021-05-26 18:25:03 +1000

    added fetch function to grab character data

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ document.querySelector('.btn').addEventListener('click',btnFunc)
 function btnFunc (e) {
     fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
 .then(response => response.json())
-// .then(data => console.log(data))
 .then(data => document.querySelector('.btn').innerHTML=data[0].quote);
+fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+.then(response => response.json())
+.then(data => document.querySelector('.author').innerHTML=data[0].character);
 }
+btnFunc()
