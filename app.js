8f8cf1eb28ebfb20e356bdc821c367b4a94a2af6
loji4 <yogawithlindao@gmail.com>
2021-11-17 06:45:38 +1100

    added comments

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+// add an event listener to the button 
 document.querySelector('.btn').addEventListener('click',btnFunc)
-        
+  
+// function for event listener. 
 function btnFunc (e) {
-    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+// send a request to simpsons api to retrieve quotes
+fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+// retrieve json from the response 
 .then(response => response.json())
+// place json(quote) into the innerHTML 
 .then(data => document.querySelector('.btn').innerHTML=data[0].quote);
+// fetch the author of the quote 
 fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
 .then(response => response.json())
+// place json(author) into the innerHTML.
 .then(data => document.querySelector('.author').innerHTML=data[0].character);
 }
 btnFunc()
